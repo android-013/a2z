ff4998d7b19482c7ebe97654c97238f8db0530dc
android-013 <azrifat013@gmail.com>
2025-02-09 14:52:21 +0600

    API

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const projects = [
         { letter: "A", name: "Age Calculator", link: "a/index.html" },
-        { letter: "B", name: "Body Index Calculator", link: "blog-homepage.html" },
+        { letter: "B", name: "Body Calculator", link: "blog-homepage.html" },
         { letter: "C", name: "Card Matching Game", link: " https://android-013.github.io/Card-Game-Front/" },
         { letter: "D", name: "Digital Clock", link: "digital-clock.html" },
         { letter: "E", name: "Expense Tracker", link: "expense-tracker.html" },
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
         { letter: "J", name: "JavaScript Calculator", link: "calculator.html" },
         { letter: "K", name: "Keyboard Event Tester", link: "keyboard-tester.html" },
         { letter: "L", name: "Loan EMI Calculator", link: "loan-calculator.html" },
-        { letter: "M", name: "Markdown Previewer", link: "markdown.html" },
+        { letter: "M", name: "Movie Search API", link: "markdown.html" },
         { letter: "N", name: "Notes App", link: "notes.html" },
         { letter: "O", name: "Online Polling System", link: "polling.html" },
         { letter: "P", name: "Password Generator", link: "password.html" },
@@ -20,11 +20,11 @@ document.addEventListener("DOMContentLoaded", () => {
         { letter: "R", name: "Rock Paper Secissor Game", link: "https://android-013.github.io/rps-front/" },
         { letter: "S", name: "Slot Machine", link: " https://android-013.github.io/Slot-machine-front/" },
         { letter: "T", name: "To-Do List App", link: "todo.html" },
-        { letter: "U", name: "URL Shortener UI", link: "url-shortener.html" },
+        { letter: "U", name: "URL Shortener API", link: "url-shortener.html" },
         { letter: "V", name: "Voting System", link: "voting.html" },
-        { letter: "W", name: "Weather App", link: "weather.html" },
+        { letter: "W", name: "Weather API", link: "weather.html" },
         { letter: "X", name: "Xylophone Web App", link: "xylophone.html" },
-        { letter: "Y", name: "YouTube Video Search", link: "youtube-search.html" },
+        { letter: "Y", name: "YouTube API", link: "youtube-search.html" },
         { letter: "Z", name: "Zoom Image on Hover", link: "zoom.html" }
     ];
 
