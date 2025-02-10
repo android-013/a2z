cfd62e175d428cb03a9bb75a5ccce97f917facaa
android-013 <azrifat013@gmail.com>
2025-02-10 19:21:23 +0600

    t link

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
         { letter: "Q", name: "Quiz App", link: "quiz.html" },
         { letter: "R", name: "Rock Paper Secissor Game", link: "https://android-013.github.io/rps-front/" },
         { letter: "S", name: "Slot Machine", link: " https://android-013.github.io/Slot-machine-front/" },
-        { letter: "T", name: "To-Do List App", link: "todo.html" },
+        { letter: "T", name: "Two hand RPS", link: "todo.html" },
         { letter: "U", name: "URL Shortener API", link: "url-shortener.html" },
         { letter: "V", name: "Voting System", link: "voting.html" },
         { letter: "W", name: "Weather API", link: "weather.html" },
