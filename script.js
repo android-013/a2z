184be64f09d7b03c840f5a0ce5c81a849aeae339
android-013 <azrifat013@gmail.com>
2025-02-14 00:30:40 +0600

    b

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const projects = [
         { letter: "A", name: "Ages Calculator", link: "a/index.html" },
-        { letter: "B", name: "Body Calculator", link: "blog-homepage.html" },
+        { letter: "B", name: "Bad UI", link: "blog-homepage.html" },
         { letter: "C", name: "Card Matching Game", link: " https://android-013.github.io/Card-Game-Front/" },
         { letter: "D", name: "Digital Clock", link: "d.html" },
         { letter: "E", name: "Enigma Machine", link: "e/index.html" },
         { letter: "F", name: "Five Way (Star trek) RPS", link: "guess-number.html" },
-        { letter: "G", name: "Guess the Number Game", link: "guess-number.html" },
-        { letter: "H", name: "Horoscope Finder", link: "horoscope.html" },
-        { letter: "I", name: "Image Gallery", link: "image-gallery.html" },
-        { letter: "J", name: "JavaScript Calculator", link: "calculator.html" },
-        { letter: "K", name: "Keyboard Event Tester", link: "keyboard-tester.html" },
-        { letter: "L", name: "Loan EMI Calculator", link: "loan-calculator.html" },
-        { letter: "M", name: "Movie Search API", link: "markdown.html" },
-        { letter: "N", name: "Notes App", link: "notes.html" },
-        { letter: "O", name: "Online Polling System", link: "polling.html" },
+        //{ letter: "G", name: "Guess the Number Game", link: "guess-number.html" },
+        //{ letter: "H", name: "Horoscope Finder", link: "horoscope.html" },
+        //{ letter: "I", name: "Image Gallery", link: "image-gallery.html" },
+        //{ letter: "J", name: "JavaScript Calculator", link: "calculator.html" },
+        //{ letter: "K", name: "Keyboard Event Tester", link: "keyboard-tester.html" },
+        //{ letter: "L", name: "Loan EMI Calculator", link: "loan-calculator.html" },
+        //{ letter: "M", name: "Movie Search API", link: "markdown.html" },
+        //{ letter: "N", name: "Notes App", link: "notes.html" },
+        //{ letter: "O", name: "Online Polling System", link: "polling.html" },
         { letter: "P", name: "Password Generator", link: "password.html" },
         { letter: "Q", name: "Quiz App", link: "quiz.html" },
         { letter: "R", name: "Rock Paper Secissor Game", link: "https://android-013.github.io/rps-front/" },
@@ -22,10 +22,10 @@ document.addEventListener("DOMContentLoaded", () => {
         { letter: "T", name: "Two hand RPS", link: "todo.html" },
         { letter: "U", name: "URL Shortener API", link: "url-shortener.html" },
         { letter: "V", name: "Voting System", link: "https://android-013.github.io/Elo-Poll/" },
-        { letter: "W", name: "Weather API", link: "weather.html" },
-        { letter: "X", name: "Xylophone Web App", link: "xylophone.html" },
-        { letter: "Y", name: "YouTube API", link: "youtube-search.html" },
-        { letter: "Z", name: "Zoom Image on Hover", link: "zoom.html" }
+        //{ letter: "W", name: "Weather API", link: "weather.html" },
+        //{ letter: "X", name: "Xylophone Web App", link: "xylophone.html" },
+        //{ letter: "Y", name: "YouTube API", link: "youtube-search.html" },
+        //{ letter: "Z", name: "Zoom Image on Hover", link: "zoom.html" }
     ];
 
     const grid = document.querySelector(".grid");
