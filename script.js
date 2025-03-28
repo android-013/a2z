8010f39515be6bea31bf29f3d95fd9c4dabf2c89
android-013 <azrifat013@gmail.com>
2025-03-28 13:17:37 +0600

    u

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
         { letter: "R", name: "Rock Paper Secissor Game", link: "https://android-013.github.io/rps-front/" },
         { letter: "S", name: "Slot Machine", link: " https://android-013.github.io/Slot-machine-front/" },
         { letter: "T", name: "Tic Tac Toe", link: "https://android-013.github.io/Tic-Tac-Toe/" },
-        //{ letter: "U", name: "URL Shortener API", link: "url-shortener.html" },
+        { letter: "U", name: "URL Shortener API", link: "https://android-013.github.io/Unit-Converter/" },
         { letter: "V", name: "Voting System", link: "https://android-013.github.io/Elo-Poll/" },
         //{ letter: "W", name: "Weather API", link: "weather.html" },
         //{ letter: "X", name: "Xylophone Web App", link: "xylophone.html" },
