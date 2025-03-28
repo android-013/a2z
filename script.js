9a8cd962ad918925f90d78418d29c99eff302bbc
android-013 <azrifat013@gmail.com>
2025-03-28 15:10:43 +0600

    v 0

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
         { letter: "S", name: "Slot Machine", link: " https://android-013.github.io/Slot-machine-front/" },
         { letter: "T", name: "Tic Tac Toe", link: "https://android-013.github.io/Tic-Tac-Toe/" },
         { letter: "U", name: "URL Shortener API", link: "https://android-013.github.io/Unit-Converter/" },
-        { letter: "V", name: "Voting System", link: "https://android-013.github.io/Elo-Poll/" },
+        { letter: "V", name: "Voting System", link: "https://android-013.github.io/Elo-Poll-Front/" },
         //{ letter: "W", name: "Weather API", link: "weather.html" },
         //{ letter: "X", name: "Xylophone Web App", link: "xylophone.html" },
         //{ letter: "Y", name: "YouTube Live", link: "youtube-search.html" },
